6193ad2081cd5fc9a67d4d715f7008614bc8b181
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-05-22 23:26:13 +0300

    [feat] Add transfer operations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ import { GetAppIdResponse } from "@/models/GetAppIdResponse";
 import { InitializeUserResponse } from "@/models/InitializeUserResponse";
 import { v4 as uuidv4 } from "uuid";
 
+const USER_TOKEN =
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoTW9kZSI6IlBJTiIsImRldmVsb3BlckVudGl0eUVudmlyb25tZW50IjoiVEVTVCIsImVudGl0eUlkIjoiMjc4Y2U3MDEtNWZlMy00MzA5LTkzZGEtOTQyYzI4NWM0NDMyIiwiZXhwIjoxNzE2NDEyNjc2LCJpYXQiOjE3MTY0MDkwNzYsImludGVybmFsVXNlcklkIjoiZjVlNjczODMtZjlmZi01YmQ5LTkxNGUtMjdhMGYzYTlkOTVkIiwiaXNzIjoiaHR0cHM6Ly9wcm9ncmFtbWFibGUtd2FsbGV0LmNpcmNsZS5jb20iLCJqdGkiOiJlMjM2N2ZlNy1jODZiLTQxYTAtOWExMi1iYWUwYzI4NWI2ZWIiLCJzdWIiOiJjMDFkOTNhMS1kZWYzLTQwZDMtOTg0MS03NjdmNzBmMzEwNDgifQ.ALQsF0hq0W42OAnv3AnaCbMSqoWq3fbNkvg8Fzf0WkYW-8Gi6Q9OAPNrzEwNzqTY0beeJRaMVK7wyYgHhRgVgetO24Z7o95xvl_hDysjntRm6gznQXpyW4Zu2BUR-4WUTHBhhvvnSrAl8WztVafI_b3A8TeHkktxsBiLt0tBTV74zJn48xPBsEfuGoKpfH0tJAuYHcYFDSrmlfQvyJqznQwj2Pnq1vTddGjfWtIc3-rEfx8f5JOjEpPDJ07zbkPHduTEG-VoYapmfzfpkLfzCIGc_jWeVBSqqhxL_XGPPePRtPG7Xf_a64e5-K8hBcAeBLGChoS5kBXxhAqkbs5Vhg";
+
+const ENCRYPTION_KEY = "Nf18trJgc/clLgudHXlkEfeG6Pq7Z7RnYFdSruLzs9U=";
+
+const USER_ID = "c01d93a1-def3-40d3-9841-767f70f31048";
+
+const WALLET_ID = "8fcf412d-f303-589e-82d7-3d3ba13b5551";
+
 export default async function Home() {
   async function getAppId() {
     const res = await fetch(`${API_URL}/config/entity`, {
@@ -40,7 +49,7 @@ export default async function Home() {
   async function createToken() {
     "use server";
 
-    const userId = "c01d93a1-def3-40d3-9841-767f70f31048";
+    const userId = USER_ID;
     const res = await fetch(`${API_URL}/users/token`, {
       method: "POST",
       headers: {
@@ -59,9 +68,8 @@ export default async function Home() {
     "use server";
     const idempotencyKey = uuidv4();
     const blockchains = ["MATIC-AMOY"];
-    const userId = "c01d93a1-def3-40d3-9841-767f70f31048";
-    const userToken =
-      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoTW9kZSI6IlBJTiIsImRldmVsb3BlckVudGl0eUVudmlyb25tZW50IjoiVEVTVCIsImVudGl0eUlkIjoiMjc4Y2U3MDEtNWZlMy00MzA5LTkzZGEtOTQyYzI4NWM0NDMyIiwiZXhwIjoxNzE2MjQwNTYyLCJpYXQiOjE3MTYyMzY5NjIsImludGVybmFsVXNlcklkIjoiZjVlNjczODMtZjlmZi01YmQ5LTkxNGUtMjdhMGYzYTlkOTVkIiwiaXNzIjoiaHR0cHM6Ly9wcm9ncmFtbWFibGUtd2FsbGV0LmNpcmNsZS5jb20iLCJqdGkiOiIxYzJhYjVlNS0yNjZjLTQ4Y2QtYmI4Yy05ZTAxZDJiMjJhYjIiLCJzdWIiOiJjMDFkOTNhMS1kZWYzLTQwZDMtOTg0MS03NjdmNzBmMzEwNDgifQ.eVKZoRD8TNoXBEKBObjKE-oH23quJLuMeLBcxwkT7jj2uWTg6yLwCP6WFCJ0ONKGSIsgxYUA6OLarU4x_Xqn0WK6UVTXdhCjbGmXk_qrLJah3oK6bPkprCOP7wMNocxvW6HWEofm95LYdmi3MeNuD2rymA8KC2T28AHMKT2ezEdoVK1q_Qu0ZPd7xGW2LFvZC9Eik0cUxvgDs-jDsIkZvyIV3rtXraP20YLqyRl_b6dbGVbH2bpBDI2kqyzM0o9vgY2u82ot791GAqXnsMPfJyiKUKQrb975cNIn55HtzNW7C9BM2q0Nbb-SB0WRhYtSIIzDA0R4cE5MaafBIeQ1hA";
+    const userId = USER_ID;
+    const userToken = USER_TOKEN;
     const res = await fetch(`${API_URL}/user/initialize`, {
       method: "POST",
       headers: {
@@ -77,6 +85,120 @@ export default async function Home() {
     const data: InitializeUserResponse = await res.json();
     console.log("ChallengeId", data.data.challengeId);
     const challengeId = "7a178c71-18b5-5478-8180-426462329bc6";
+  }
+
+  async function getUserStatus() {
+    "use server";
+    const idempotencyKey = uuidv4();
+    const blockchains = ["MATIC-AMOY"];
+    const userId = USER_ID;
+    const userToken = USER_TOKEN;
+    const res = await fetch(`${API_URL}/user`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+    });
+    const data = await res.json();
+    console.log("DATA", data);
+  }
+
+  async function getWallets() {
+    "use server";
+    const userToken = USER_TOKEN;
+    const res = await fetch(`${API_URL}/wallets`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+    });
+    const data = await res.json();
+    console.log("WALLETS", data.data.wallets);
+  }
+
+  async function transactions() {
+    "use server";
+    const userToken = USER_TOKEN;
+    const res = await fetch(`${API_URL}/transactions`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+    });
+    const data = await res.json();
+    console.log("TRANSACTIONS", data.data);
+  }
+  async function balances() {
+    "use server";
+    const userToken = USER_TOKEN;
+    const res = await fetch(`${API_URL}/wallets/${WALLET_ID}/balances`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+    });
+    const data = await res.json();
+    console.log("BALANCES", data.data);
+  }
+
+  async function estimateFee() {
+    "use server";
+    const idempotencyKey = uuidv4();
+    const blockchains = ["MATIC-AMOY"];
+    const userId = USER_ID;
+    const userToken = USER_TOKEN;
+    const body = {
+      amounts: [".01"],
+      destinationAddress: "0x48520ff9b32d8b5bf87abf789ea7b3c394c95ebe",
+      tokenId: "36b6931a-873a-56a8-8a27-b706b17104ee",
+      walletId: "8fcf412d-f303-589e-82d7-3d3ba13b5551",
+    };
+    const res = await fetch(`${API_URL}/transactions/transfer/estimateFee`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+      body: JSON.stringify(body),
+    });
+    const data: InitializeUserResponse = await res.json();
+    console.log("Estimation", data.data);
+  }
+  async function transfer() {
+    "use server";
+    const idempotencyKey = uuidv4();
+    const blockchains = ["MATIC-AMOY"];
+    const userId = USER_ID;
+    const userToken = USER_TOKEN;
+    const body = {
+      userId: USER_ID,
+      idempotencyKey: idempotencyKey,
+      amounts: [".01"],
+      destinationAddress: "0x48520ff9b32d8b5bf87abf789ea7b3c394c95ebe",
+      tokenId: "36b6931a-873a-56a8-8a27-b706b17104ee",
+      walletId: "8fcf412d-f303-589e-82d7-3d3ba13b5551",
+      feeLevel: "MEDIUM",
+    };
+    const res = await fetch(`${API_URL}/user/transactions/transfer`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+        "X-User-Token": userToken,
+      },
+      body: JSON.stringify(body),
+    });
+    const data: InitializeUserResponse = await res.json();
+    console.log("transfer", data);
   }
 
   const appId = await getAppId();
@@ -98,6 +220,36 @@ export default async function Home() {
       <form action={initializeUser}>
         <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
           InitializeUser
+        </button>
+      </form>
+      <form action={getUserStatus}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          Get User Status
+        </button>
+      </form>
+      <form action={getWallets}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          Get Wallets
+        </button>
+      </form>
+      <form action={transactions}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          Transactions
+        </button>
+      </form>
+      <form action={balances}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          Balances
+        </button>
+      </form>
+      <form action={estimateFee}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          EstimateFee
+        </button>
+      </form>
+      <form action={transfer}>
+        <button className="bg-blue-500 px-4 py-2 rounded-lg" type="submit">
+          Transfer
         </button>
       </form>
       <hr />
